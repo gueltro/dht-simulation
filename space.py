377f271843cf07264d570aef4410702164918078
gueltro <gueltro@gmail.com>
2015-07-12 14:28:15 -0700

    created space class

---
+++ space.py
@@ -0,0 +1,24 @@
+# Create space where dht will be embedded  
+
+def Space():
+    
+    def __init__():
+        self.constraints=[]
+    
+    # Raise an error if the location is not in space
+    def in_space(self,location)
+        const = self.constraints
+        num_contrains = len(const)
+        num_coordinates = len(location)
+        
+        assert num_coordinates = num_locations
+
+        for i in num_contrains:
+            coord = location[i]
+            lower = const[0][1]
+            upper = const[i][1]
+            
+            in_boundary = (lower < coord) and (lower > coord)
+            
+            assert in_boundary 
+            
